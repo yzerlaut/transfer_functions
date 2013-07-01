5c4d6a7c99ea341f1d46eb408b7db1cb6d506404
yzerlaut <yann.zerlaut@gmail.com>
2013-07-02 00:52:08 +0200

    file for the analytic formule

---
+++ analytical_formula.py
@@ -0,0 +1,98 @@
+#!/usr/bin/python
+# Filename: analytical_formula.py
+
+import numpy as np
+from scipy import integrate
+
+def tau_eff(fe,fi,params):
+    """
+    args : params
+    returns : the efective membrane time constant
+    """
+    Cm = params['Cm'];Gl=params['Gl'];El=params['El']
+    Qe = params['Qe'];Ne = params['Ne'];Te=params['Te'];Ee=params['Ee']
+    Qi = params['Qi'];Ni = params['Ni'];Ti=params['Ti'];Ei=params['Ei']
+
+    ge=fe*Ne*Qe*Te
+    gi=fi*Ni*Qi*Ti
+    return Cm/(Gl+ge+gi)
+
+def campbell_theorem_single_exp(f, Q, T):
+    """
+    returns the mean and variance
+    of a shotnoise convoluted by a single exponential
+    args : f, Q, T // frequency, quantal, time decay
+    returns : mean and variance
+    """
+    mu = f*Q*T
+    sigma =  Q*np.sqrt(f*T/2)
+    return mu, sigma
+
+def rudolph_rho_v(V, param_rudolph):
+    """
+    args : v, param_rudolph
+    param_rudolph = [Cm, Gl, El, mu_ge, s_ge, Te, Ee, mu_gi, s_gi, Ti, Ei]
+    returns : the distribution for the membrane potential as the one derived
+    in Rudolph et al. 2005 (with corrections)
+    !!! IT NEEDS TO BE RENORMALIZED !!!
+    """
+    Cm = param_rudolph['Cm'];Gl=param_rudolph['Gl'];El=param_rudolph['El']
+    ge = param_rudolph['mu_ge'];sge = param_rudolph['s_ge'];Te=param_rudolph['Te'];Ee=param_rudolph['Ee']
+    gi = param_rudolph['mu_gi'];sgi = param_rudolph['s_gi'];Ti=param_rudolph['Ti'];Ei=param_rudolph['Ei']
+
+    np.where(ge<=0,ge,1e-4);np.where(gi<=0,gi,1e-4)# security in the calculus (see A2)
+    Tm = Cm/(ge+gi+Gl) # effective membrane time constant
+
+    Te = 2*Te*Tm/(Te+Tm) # correction
+    Ti = 2*Ti*Tm/(Ti+Tm) # see rudolph et al. 2005
+
+    A1 = - (2*Cm*(ge+gi)+2*Cm*Gl+sge**2*Te+sgi**2*Ti)
+    A1 /= 2*(sge**2*Te+sgi**2*Ti)
+    A2 = 2*Cm*(Gl*(sge**2*Te*(El-Ee)+sgi**2*Ti*(El-Ei))+\
+               (ge*sgi**2*Ti-gi*sge**2*Te)*(Ee-Ei))
+    A2 /= (Ee-Ei)*np.sqrt(sge**2*Te*sgi**2*Ti)*(sge**2*Te+sgi**2*Ti)
+    a_ln = sge**2*Te/Cm*(V-Ee)**2+sgi**2*Ti/Cm*(V-Ei)**2
+    a_arctan = sge**2*Te*(V-Ee)+sgi**2*Ti*(V-Ei)
+    a_arctan /= (Ee-Ei)*np.sqrt(sge**2*Te*sgi**2*Ti)
+    return np.exp(A1*np.log(a_ln)+A2*np.arctan(a_arctan))
+
+def rudolph_rho_normed(V,param_rudolph):
+    integrand = lambda x: rudolph_rho_v(x, param_rudolph)
+    N,error = integrate.quad(integrand,-np.inf,np.inf) # normalization factor
+    print "absolute error on the function :",error
+    return rudolph_rho_v(V, param_rudolph)/N
+
+def rudolph_rho_normed_shotnoise(V, fe, fi, params):
+    """
+     here we return the same as in 'rudolph_rho_normed',
+    we just adapt the formalism so that it takes shot-noise parameters
+    """
+    # here we copy the aparams dict() !!!
+    param_rudolph['mu_ge'], param_rudolph['s_ge'] = \
+      tf.campbell_theorem_single_exp(
+          params['Ne']*fe, params['Qe'], params['Te'])
+    param_rudolph['mu_gi'], param_rudolph['s_gi'] = \
+      tf.campbell_theorem_single_exp(
+          params['Ni']*fi, params['Qi'], params['Ti'])
+
+    return rudolph_rho_normed(V, param_rudolph)
+
+def proba_rudolph(fe,fi,params):
+    threshold = params['Vthre']
+    # version with analytic integration
+    integrand = lambda x: rudolph_rho_v(x,fe,fi,params)
+    N,error = integrate.quad(integrand,-np.inf,np.inf) # normalization factor
+    return integrate.quad(integrand,threshold,np.inf)[0]/N
+    """
+    # version with discrete integration
+    vv = np.linspace(threshold,0,1e2)
+    distr = rudolph_rho_normed(vv,fe,fi,params)
+    return integrate.cumtrapz(distr,vv)[0]
+    """
+
+def kuhn_func(fe,fi,params):
+    return proba_rudolph(fe,fi,params)/tau_eff(fe,fi,params)
+    
+    
+        
+
